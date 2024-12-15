4d2670f8380f94b21c8d64945926ba2f5f3850bb
FUFU222 <tnkakr_0817@icloud.com>
2024-12-16 08:20:52 +0900

    update

--- app/api/purchases/[userid]/route.ts
+++ app/api/purchases/[userid]/route.ts
@@ -1,40 +1,40 @@
-import prisma from "@/app/lib/next-auth/prisma";
-import { NextResponse } from "next/server";
-
-export async function GET(
-  request: Request, 
-  { params }: { params: { userid: string } }
-) {
-  console.log(params.userid)
-  const userid = params.userid;
-
-  try {
-    const purchases = await prisma.purchase.findMany({
-      where: { userId: userid },
-    })
-    return NextResponse.json(purchases);
-  } catch (err) {
-    return NextResponse.json(err)
-  }
-}
 // import prisma from "@/app/lib/next-auth/prisma";
 // import { NextResponse } from "next/server";
 
 // export async function GET(
-//   request: Request,
-//   {params}: { params: Promise<{ userid: string }>} // 正しい型
+//   request: Request, 
+//   { params }: { params: { userid: string } }
 // ) {
-//   const { userid } = await params;// 動的ルートのパラメータを取得
+//   console.log(params.userid)
+//   const userid = params.userid;
 
 //   try {
-//     // データベースから該当ユーザーの購入履歴を取得
 //     const purchases = await prisma.purchase.findMany({
 //       where: { userId: userid },
-//     });
-
-//     return NextResponse.json(purchases); // 購入履歴をJSONで返す
+//     })
+//     return NextResponse.json(purchases);
 //   } catch (err) {
-//     console.error("Error fetching purchases:", err);
-//     return NextResponse.json({ error: "Failed to fetch purchases" }, { status: 500 });
+//     return NextResponse.json(err)
 //   }
 // }
+import prisma from "@/app/lib/next-auth/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  request: Request,
+  context: {params: { userid: string } },
+) {
+  const { userid } = context.params;// 動的ルートのパラメータを取得
+
+  try {
+    // データベースから該当ユーザーの購入履歴を取得
+    const purchases = await prisma.purchase.findMany({
+      where: { userId: userid },
+    });
+
+    return NextResponse.json(purchases); // 購入履歴をJSONで返す
+  } catch (err) {
+    console.error("Error fetching purchases:", err);
+    return NextResponse.json({ error: "Failed to fetch purchases" }, { status: 500 });
+  }
+}
