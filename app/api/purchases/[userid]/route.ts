09b1fdaa230ed4eadafcea79e570400e0725dfeb
FUFU222 <tnkakr_0817@icloud.com>
2024-12-18 09:23:45 +0900

    update

--- app/api/purchases/[userid]/route.ts
+++ app/api/purchases/[userid]/route.ts
@@ -1,22 +1,3 @@
-// import prisma from "@/app/lib/next-auth/prisma";
-// import { NextResponse } from "next/server";
-
-// export async function GET(
-//   request: Request, 
-//   { params }: { params: { userid: string } }
-// ) {
-//   console.log(params.userid)
-//   const userid = params.userid;
-
-//   try {
-//     const purchases = await prisma.purchase.findMany({
-//       where: { userId: userid },
-//     })
-//     return NextResponse.json(purchases);
-//   } catch (err) {
-//     return NextResponse.json(err)
-//   }
-// }
 import prisma from "@/app/lib/next-auth/prisma";
 import { NextResponse } from "next/server";
 
